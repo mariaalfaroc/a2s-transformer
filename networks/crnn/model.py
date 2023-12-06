168959f7ad982ac53133583e1a3c2530adbf58ce
mariaalfaroc <mac77@alu.ua.es>
2023-12-06 08:09:50 +0100

    added crnn model

---
+++ networks/crnn/model.py
@@ -0,0 +1,94 @@
+import random
+
+import torch
+from torch.nn import CTCLoss
+from torchinfo import summary
+from lightning.pytorch import LightningModule
+
+from networks.crnn.modules import CRNN
+from my_utils.metrics import compute_metrics
+from my_utils.data_preprocessing import IMG_HEIGHT, NUM_CHANNELS
+
+
+class CTCTrainedCRNN(LightningModule):
+    def __init__(self, w2i, i2w, ytest_i2w=None, num_frame_repeats=16):
+        super(CTCTrainedCRNN, self).__init__()
+        # Save hyperparameters
+        self.save_hyperparameters()
+        # Dictionaries
+        self.w2i = w2i
+        self.i2w = i2w
+        self.ytest_i2w = ytest_i2w if ytest_i2w is not None else i2w
+        # Model
+        self.model = CRNN(
+            output_size=len(self.w2i) + 1, num_frame_repeats=num_frame_repeats
+        )
+        self.summary()
+        # Loss: the target index cannot be blank!
+        self.compute_ctc_loss = CTCLoss(blank=len(self.w2i), zero_infinity=False)
+        # Predictions
+        self.Y = []
+        self.YHat = []
+
+    def summary(self):
+        summary(self.model, input_size=[1, NUM_CHANNELS, IMG_HEIGHT, 256])
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.model.parameters(), lr=1e-3, weight_decay=1e-6)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        x, xl, y, yl = batch
+        yhat = self.model(x)
+        # ------ CTC Requirements ------
+        # yhat: [batch, frames, vocab_size]
+        yhat = yhat.log_softmax(dim=2)
+        yhat = yhat.permute(1, 0, 2).contiguous()
+        # ------------------------------
+        loss = self.compute_ctc_loss(yhat, y, xl, yl)
+        self.log("train_loss", loss, prog_bar=True, logger=True, on_epoch=True)
+        return loss
+
+    def ctc_greedy_decoder(self, y_pred, i2w):
+        # y_pred = [seq_len, num_classes]
+        # Best path
+        y_pred_decoded = torch.argmax(y_pred, dim=1)
+        # Merge repeated elements
+        y_pred_decoded = torch.unique_consecutive(y_pred_decoded, dim=0).tolist()
+        # Convert to string; len(i2w) -> CTC-blank
+        y_pred_decoded = [i2w[i] for i in y_pred_decoded if i != len(i2w)]
+        return y_pred_decoded
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch  # batch_size = 1
+        # Model prediction (decoded using the vocabulary on which it was trained)
+        yhat = self.model(x)[0]
+        yhat = yhat.log_softmax(dim=-1).detach().cpu()
+        yhat = self.ctc_greedy_decoder(yhat, self.i2w)
+        # Decoded ground truth
+        y = [self.ytest_i2w[i.item()] for i in y[0]]
+        # Append to later compute metrics
+        self.Y.append(y)
+        self.YHat.append(yhat)
+
+    def test_step(self, batch, batch_idx):
+        return self.validation_step(batch, batch_idx)
+
+    def on_validation_epoch_end(self, name="val", print_random_samples=False):
+        metrics = compute_metrics(y_true=self.Y, y_pred=self.YHat)
+        for k, v in metrics.items():
+            self.log(f"{name}_{k}", v, prog_bar=True, logger=True, on_epoch=True)
+        # Print random samples
+        if print_random_samples:
+            index = random.randint(0, len(self.Y) - 1)
+            print(f"Ground truth - {self.Y[index]}")
+            print(f"Prediction - {self.YHat[index]}")
+        # Clear predictions
+        self.Y.clear()
+        self.YHat.clear()
+        return metrics
+
+    def on_test_epoch_end(self):
+        return self.on_validation_epoch_end(name="test", print_random_samples=True)
---
+++ networks/crnn/modules.py
@@ -0,0 +1,83 @@
+import torch
+import torch.nn as nn
+
+from my_utils.data_preprocessing import NUM_CHANNELS, IMG_HEIGHT
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        layers = [
+            # First block
+            nn.Conv2d(NUM_CHANNELS, 8, (10, 2), padding="same", bias=False),
+            nn.BatchNorm2d(8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 2)),
+            # Second block
+            nn.Conv2d(8, 8, (8, 5), padding="same", bias=False),
+            nn.BatchNorm2d(8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.MaxPool2d((2, 1)),
+        ]
+        self.backbone = nn.Sequential(*layers)
+        self.width_reduction = 2
+        self.height_reduction = 2**2
+        self.out_channels = 8
+
+    def forward(self, x):
+        return self.backbone(x)
+
+
+class RNN(nn.Module):
+    def __init__(self, input_size: int, output_size: int):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.blstm = nn.LSTM(
+            self.input_size,
+            256,
+            num_layers=2,
+            batch_first=True,
+            dropout=0.5,
+            bidirectional=True,
+        )
+        self.dropout = nn.Dropout(0.5)
+        self.linear = nn.Linear(256 * 2, output_size)
+
+    def forward(self, x):
+        b, _, _, _ = x.size()
+        x = x.permute(0, 3, 2, 1).contiguous()
+        x = x.reshape(b, -1, self.input_size)
+        x, _ = self.blstm(x)
+        x = self.dropout(x)
+        x = self.linear(x)
+        return x
+
+
+class CRNN(nn.Module):
+    def __init__(self, output_size: int, num_frame_repeats: int = 16):
+        super(CRNN, self).__init__()
+        # CNN
+        self.cnn = CNN()
+        # RNN
+        self.num_frame_repeats = num_frame_repeats
+        self.rnn_input_size = self.cnn.out_channels * (
+            IMG_HEIGHT // self.cnn.height_reduction
+        )
+        self.rnn = RNN(input_size=self.rnn_input_size, output_size=output_size)
+
+    def forward(self, x):
+        # CNN
+        # x: [b, NUM_CHANNELS, IMG_HEIGHT, w]
+        x = self.cnn(x)
+        # x: [b, self.cnn.out_channels, nh = IMG_HEIGHT // self.height_reduction, nw = w // self.width_reduction]
+        # Prepare for RNN
+        b, _, _, w = x.size()
+        x = x.permute(0, 3, 1, 2).contiguous()
+        x = x.reshape(b, w, self.rnn_input_size)
+        # x: [b, nw, self.cnn.out_channels * nh]
+        x = torch.repeat_interleave(x, repeats=self.num_frame_repeats, dim=1)
+        # x: [b, self.num_frame_repeats * nw, self.cnn.out_channels * nh]
+        # RNN
+        x = self.rnn(x)
+        # x: [b, self.num_frame_repeats * nw, output_size]
+        return x
